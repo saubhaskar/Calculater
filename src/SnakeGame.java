0f8a8b2830e7c0bac7226b2d60a25aa2abd56ffd
Anonymous <AnonymousMyComputer@gmail.com>
2022-12-27 17:22:49 +0530

    Initial commit

---
+++ src/SnakeGame.java
@@ -0,0 +1,12 @@
+
+
+public class SnakeGame {
+    public void main(String[] args) {
+        new GameFrame();
+//        GamePanel panel = new GamePanel();
+        this.add(new GamePanel());
+    }
+
+    private void add(GamePanel gamePanel) {
+    }
+}
